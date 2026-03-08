3627f01030f8ed82e4ca28d8f0f8ea47bc76759b
Febry Ardiansyah <febrymuhammad80@gmail.com>
2026-03-08 12:35:53 +0700

    fix: model

--- lib/data/models/post_model.dart
+++ lib/data/models/post_model.dart
@@ -6,19 +6,18 @@ import '../../domain/entities/post.dart';
 /// from/to JSON, specifically for the data layer.
 class PostModel extends Post {
   /// Creates a [PostModel] with the given [id], [title], and [body].
-  const PostModel({required int id, required String title, required String body})
-      : super(id: id, title: title, body: body);
+  const PostModel({
+    required super.id,
+    required super.title,
+    required super.body,
+  });
 
   /// Creates a [PostModel] from a JSON map.
   ///
   /// This factory method is used to parse the JSON response from the API
   /// into a [PostModel] object.
   factory PostModel.fromJson(Map<String, dynamic> json) {
-    return PostModel(
-      id: json['id'],
-      title: json['title'],
-      body: json['body'],
-    );
+    return PostModel(id: json['id'], title: json['title'], body: json['body']);
   }
 
   /// Converts this [PostModel] into a JSON map.
@@ -26,10 +25,6 @@ class PostModel extends Post {
   /// This method is useful for sending data to an API, though not directly
   /// used in this read-only example.
   Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'title': title,
-      'body': body,
-    };
+    return {'id': id, 'title': title, 'body': body};
   }
 }
